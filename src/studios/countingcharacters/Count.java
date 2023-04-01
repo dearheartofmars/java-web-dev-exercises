42dffb9744c8db59b024bec9fa19257b59748faa
dearheartofmars <mnlebeau@gmail.com>
2023-04-01 16:28:03 -0500

    commit final

--- src/studios/countingcharacters/Count.java
+++ src/studios/countingcharacters/Count.java
@@ -6,18 +6,20 @@ public class Count {
     public static void main(String[] args) {
         HashMap<Character, Integer > ToCount = new HashMap<>();
         String word = "onomatopoeia";
-        char[] charactersInString = word.toCharArray();
-        for (char i : charactersInString) {
+//        char[] charactersInString = word.toCharArray();
+        for (char i : word.toLowerCase().toCharArray()) {
            if(ToCount.containsKey(i)) {
+               ToCount.put(i, ToCount.get(i) + 1);
            } else {
-                   ToCount.put(i , 1);
+               ToCount.put(i , 1);
                }
-               System.out.println(ToCount);
+        }
+        for(Map.Entry<Character, Integer> characters : ToCount.entrySet()) {
+            System.out.println(characters.getKey() + ":" + characters.getValue() );
         }
             }
 
         }
-
 
 
 
