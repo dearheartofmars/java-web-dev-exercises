06283c11a26db14731f3b509fbd9a25db0f36784
dearheartofmars <mnlebeau@gmail.com>
2023-04-10 18:41:45 -0500

    car exercises

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -4,10 +4,52 @@ package org.launchcode.java.demos.lsn3classes1;
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 public class Student {
-
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits;
+    private double gpa;
+
+
+//    private String name;
+    public String getName(){
+        return name;
+    }
+    public String setName(){
+        this.name = name;
+        return null;
+    }
+//    private int studentId;
+
+    public int getStudentId(){
+        return studentId;
+    }
+
+    public void setStudentId(){
+        this.studentId = studentId;
+    }
+//    private int numberOfCredits = 0;
+
+    private int getNumberOfCredits(){
+        return numberOfCredits;
+    }
+
+    private void setNumberOfCredits(int NumberOfCredits){
+        this.numberOfCredits = numberOfCredits;
+    }
+//    private double gpa = 0.0;
+
+    public double getGpa(){
+        return gpa;
+    }
+    public void setGpa(){
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this.name = "Mars";
+        this.studentId = 123456;
+        this.numberOfCredits = 1;
+        this.gpa = 4.0;
+    }
 
 }
